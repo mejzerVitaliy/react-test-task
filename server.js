575094ac7688b59bfc09fa798064631fcdf60a04
mejzerVitaliy <mejzervitalik@gmail.com>
2024-08-29 15:40:14 +0300

    finally commit #2

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
