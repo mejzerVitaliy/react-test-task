3ca1cedbdbe20a7249cdd0c548b1b9a44011e637
mejzerVitaliy <mejzervitalik@gmail.com>
2024-08-29 18:00:47 +0300

    --------

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions)); 
 
-app.use('/api', middlewares, router);
+app.use( middlewares, router);
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
