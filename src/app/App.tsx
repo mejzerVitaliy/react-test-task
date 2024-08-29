7d640d49395ae5b2023eecbf194bd3b9bb8a529f
mejzerVitaliy <mejzervitalik@gmail.com>
2024-08-29 19:15:59 +0300

    +

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { BrowserRouter, useNavigate } from 'react-router-dom'
 import AppRouter from './router/AppRouter'
 
-const App:React.FC = () => {
+const App: React.FC = () => {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        navigate('https://my-react-test-task.onrender.com')
+    }, [navigate])
+
     return (
         <BrowserRouter>
             <AppRouter />
