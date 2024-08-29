af958416e9bb90d7bd519c9f87a47f4043dbcde2
mejzerVitaliy <mejzervitalik@gmail.com>
2024-08-29 17:19:07 +0300

    qwe

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import jsonServer from 'json-server';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import cors from 'cors';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,15 +11,7 @@ const app = express();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
 
-
-app.use(cors());
 app.use('/api', middlewares, router);
 
 app.use(express.static(path.join(__dirname, 'dist')));
