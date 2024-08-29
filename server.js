7f7b9ae787ce47f339e6299cb9443e580c26c091
mejzerVitaliy <mejzervitalik@gmail.com>
2024-08-29 15:50:10 +0300

    finally changes

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.get('*', (req, res) => {
 });
 
 const PORT = process.env.PORT || 10000;
+const HOST = '0.0.0.0'
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://${HOST}:${PORT}`);
 });
