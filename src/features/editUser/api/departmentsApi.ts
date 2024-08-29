9aea874e336331975671059adfc66cbd7ca15e54
mejzerVitaliy <mejzervitalik@gmail.com>
2024-08-29 15:23:53 +0300

    finaly build

--- src/features/editUser/api/departmentsApi.ts
+++ src/features/editUser/api/departmentsApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
-const base_url = 'http://localhost:5000'
+const base_url = 'http://localhost:10000'
 
 export const departmentsApi = createApi({
     reducerPath: 'departmentsApi',
